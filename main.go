98f5d5a9f95e1c5e006950fb94fa91bea774dfc7
Shail Patel <34988167+shailp250@users.noreply.github.com>
2021-05-07 21:15:25 -0400

    Add files via upload

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/shailp250/Golang-BlockChain/Golang-BlockChain/blockchain"
 )
 
 func main() {
@@ -11,9 +14,14 @@ func main() {
 	chain.AddBlockToChain("Third block after Init")
 	chain.AddBlockToChain("Fourth block after Init")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Prev hash: %x\n", block.PrevHash)
 		fmt.Printf("data in block: %s\n", block.Data)
 		fmt.Printf("hash: %x \n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 }
